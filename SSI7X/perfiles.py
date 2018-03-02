15e6b094e5ea63851fef1e668d9a61a91c261c57
LUIS.ARAGON <LUIS.ARAGON@LP-SISTEMAS-08.LAN.GRUPOSSI.COM>
2018-03-02 14:54:01 -0500

    gestion perfiles

--- SSI7X/perfiles.py
+++ SSI7X/perfiles.py
@@ -16,7 +16,13 @@ import SSI7X.Static.config_DB as dbConf # @UnresolvedImport
 import SSI7X.Static.config as conf  # @UnresolvedImport
 import time,json,jwt
 
-##clase de llamado para validar datos desde labels
+
+#Declaracion de variables globales
+Utils = Utils()
+lc_cnctn = ConnectDB()
+fecha_act = time.ctime()
+
+#clase de llamado para validar datos desde labels
 class DatosPerfil(Form):
     cdgo    = StringField(labels.lbl_cdgo_prfl,[validators.DataRequired(message=errors.ERR_NO_Cdgo)])
     dscrpcn = StringField(labels.lbl_dscrpcn_prfl,[validators.DataRequired(message=errors.ERR_NO_Dscrpcn)])
@@ -27,18 +33,15 @@ class DatosUpdate(Form):
     id_prfl_une = IntegerField(labels.lbl_id_prfl,[validators.DataRequired(message=errors.ERR_NO_ID)])    
         
 class Perfiles(Resource):
-    Utils = Utils()
-    lc_cnctn = ConnectDB()
-    fecha_act = time.ctime()
-    
+  
     def post(self,**kwargs):
         
         if kwargs['page']=='crear':
             return self.crear()
-        if kwargs['page']=='ListarPerfiles':
-            return self.ListarPerfiles()
-        if kwargs['page']=='ActualizarPerfil':
-            return self.ActualizarPerfil()
+        if kwargs['page']=='listar':
+            return self.listar()
+        if kwargs['page']=='actualizar':
+            return self.actualizar()
     
     def crear(self):
         lob_rspsta = DatosPerfil(request.form)
@@ -46,6 +49,7 @@ class Perfiles(Resource):
             return self.Utils.nice_json({"error":lob_rspsta.errors},400)
         
         ln_opcn_mnu = request.form["id_mnu_ge"]
+        ln_id_undd_ngco = request.form["id_undd_ngco"]
         token = request.headers['Authorization']
         validacionSeguridad = ValidacionSeguridad()
         
@@ -56,22 +60,28 @@ class Perfiles(Resource):
             arrayValues={}
             arrayValues['cdgo'] = request.form["cdgo"]
             arrayValues['dscrpcn'] = request.form["dscrpcn"]
-            ld_id_prfl =  self.lc_cnctn.queryInsert(dbConf.DB_SHMA+".tbperfiles", arrayValues,'id')
-            if ld_id_prfl:
+            ##validacion de datos repetidos
+            Cursor = lc_cnctn.querySelect(dbConf.DB_SHMA +'.tbperfiles', 'cdgo', "cdgo='"+str(arrayValues['cdgo'])+"' or dscrpcn like '%"+str(arrayValues['dscrpcn'])+"%' ")
+            if Cursor :
+                return Utils.nice_json({"error":errors.ERR_RGSTRO_RPTDO},400)
+                        
+            ln_id_prfl =  lc_cnctn.queryInsert(dbConf.DB_SHMA+".tbperfiles", arrayValues,'id')
+            if ln_id_prfl:
                 arrayValuesDetalle={}
-                arrayValuesDetalle['id_prfl'] = str(ld_id_prfl)
-                arrayValuesDetalle['id_undd_ngco'] = str(3)
+                arrayValuesDetalle['id_prfl'] = str(ln_id_prfl)
+                arrayValuesDetalle['id_undd_ngco'] = str(ln_id_undd_ngco)
                 arrayValuesDetalle['id_lgn_crcn_ge'] = str(datosUsuario['id_lgn_ge'])
                 arrayValuesDetalle['id_lgn_mdfccn_ge'] = str(datosUsuario['id_lgn_ge'])  
                 arrayValuesDetalle['fcha_mdfccn'] = str(self.fecha_act)
                 arrayValuesDetalle['fcha_mdfccn'] = str(self.fecha_act)
-                self.lc_cnctn.queryInsert(dbConf.DB_SHMA+".tbperfiles_une", arrayValuesDetalle)
+                ln_id_prfl_une = self.lc_cnctn.queryInsert(dbConf.DB_SHMA+".tbperfiles_une", arrayValuesDetalle,'id')
+                return Utils.nice_json({"success":labels.SCCSS_RGSTRO_EXTSO,"id":str(ln_id_prfl_une)},200)
             else:    
-                return self.Utils.nice_json({"error":"null"},400)    
+                return Utils.nice_json({"error":errors.ERR_PRBLMS_GRDR},400)      
         else:
-            return self.Utils.nice_json({"error":"null"},400)
+            return Utils.nice_json({"error":errors.ERR_NO_ATRZCN},400)       
         
-    def ListarPerfiles(self):
+    def listar(self):
         
         ln_opcn_mnu = request.form["id_mnu_ge"]
         token = request.headers['Authorization']
@@ -102,16 +112,16 @@ class Perfiles(Resource):
                                     " where "\
                                     " b.id_undd_ngco = "+str(ln_id_undd_ngco) +" "+ lc_dta +""\
                                     " order by a.dscrpcn"
-            Cursor = self.lc_cnctn.queryFree(strSql)    
+            Cursor = lc_cnctn.queryFree(strSql)    
             if Cursor :    
                 data = json.loads(json.dumps(Cursor, indent=2))
-                return self.Utils.nice_json(data,200)
+                return Utils.nice_json(data,200)
             else:
-                return self.Utils.nice_json({"error":labels.INFO_NO_DTS},200)
+                return Utils.nice_json({"success":labels.INFO_NO_DTS},200)
         else:
-            return self.Utils.nice_json({"error":"null"},400)
+            return Utils.nice_json({"error":errors.ERR_NO_ATRZCN},400)       
         
-    def ActualizarPerfil(self):
+    def actualizar(self):
         
         lob_rspsta = DatosUpdate(request.form)
         if not lob_rspsta.validate(): 
@@ -121,7 +131,7 @@ class Perfiles(Resource):
         token = request.headers['Authorization']
         validacionSeguridad = ValidacionSeguridad()
         
-        if validacionSeguridad.Principal(token, ln_opcn_mnu):
+        if validacionSeguridad.Principal(token, ln_opcn_mnu,optns.OPCNS_MNU['Perfiles']):
             DatosUsuarioToken = jwt.decode(token, conf.SS_TKN_SCRET_KEY, 'utf-8')
             datosUsuario = validacionSeguridad.ObtenerDatosUsuario(DatosUsuarioToken['lgn'])[0]    
                             
@@ -129,16 +139,22 @@ class Perfiles(Resource):
             lc_dscrpcn  = request.form["dscrpcn"]  
             ln_id_prfl_une = request.form["id_prfl_une"]
             lb_estdo    = request.form["estdo"]   
-               
+            
+            CursorValidar = lc_cnctn.querySelect(dbConf.DB_SHMA +'.tbperfiles', 'id'," cdgo = '"+str(lc_cdgo)+"' or dscrpcn like '%"+str(lc_dscrpcn)+"%'")
+            print(CursorValidar)                    
+            if CursorValidar:
+                print('dupicado')
+                return Utils.nice_json({"error":errors.ERR_RGSTRO_RPTDO},400)       
+                        
             arrayValues={}
             arrayValuesDetalle={}
             #Actualizo tabla une
             arrayValuesDetalle['id_lgn_mdfccn_ge']  =  str(datosUsuario['id_lgn_ge'])  
             arrayValuesDetalle['estdo']             =  lb_estdo            
             arrayValuesDetalle['fcha_mdfccn']       =  str(self.fecha_act)               
-            self.lc_cnctn.queryUpdate(dbConf.DB_SHMA+"."+str('tbperfiles_une'), arrayValuesDetalle,'id='+str(ln_id_prfl_une))
+            lc_cnctn.queryUpdate(dbConf.DB_SHMA+"."+str('tbperfiles_une'), arrayValuesDetalle,'id='+str(ln_id_prfl_une))
             #obtengo id_lgn a partir del id_lgn_ge
-            Cursor = self.lc_cnctn.querySelect(dbConf.DB_SHMA +'.tbperfiles_une', 'id_prfl', "id="+ln_id_prfl_une)
+            Cursor = lc_cnctn.querySelect(dbConf.DB_SHMA +'.tbperfiles_une', 'id_prfl', "id="+ln_id_prfl_une)
             if Cursor :
                 data        = json.loads(json.dumps(Cursor[0], indent=2))
                 ln_id_prfl  = data['id_prfl']
@@ -147,6 +163,9 @@ class Perfiles(Resource):
                 arrayValues['cdgo']   = str(lc_cdgo)
                 arrayValues['dscrpcn']= lc_dscrpcn
                 arrayValues['estdo']  = lb_estdo
-                self.lc_cnctn.queryUpdate(dbConf.DB_SHMA+"."+str('tbperfiles'), arrayValues,'id='+str(ln_id_prfl))
+                lc_cnctn.queryUpdate(dbConf.DB_SHMA+"."+str('tbperfiles'), arrayValues,'id='+str(ln_id_prfl))
+                return Utils.nice_json({"success":labels.SCCSS_ACTLZCN_EXTSA},200) 
+            else:    
+                return Utils.nice_json({"error":errors.ERR_PRBLMS_GRDR},400)
         else:
-            return self.Utils.nice_json({"error":"null"},400)
+            return Utils.nice_json({"error":errors.ERR_NO_ATRZCN},400)       
