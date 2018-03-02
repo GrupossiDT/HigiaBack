56056f0e00e0141938d3d107353efbd7eea43ef7
oscar.daza <programador2@grupossi.com>
2018-03-02 14:41:56 -0500

    gestion_preguntas_seguridad (documentacion codigo)

--- SSI7X/preguntas.py
+++ SSI7X/preguntas.py
@@ -3,176 +3,257 @@ Created on 24 ene. 2018
 
 @author: oscar.daza
 '''
+
+
+import time, json, jwt
+
 from flask_restful import request, Resource
 from wtforms import Form, validators, StringField
+
 from SSI7X.Static.ConnectDB import ConnectDB  # @UnresolvedImport
 from SSI7X.Static.Utils import Utils  # @UnresolvedImport
+import SSI7X.Static.config as conf  # @UnresolvedImport 
+import SSI7X.Static.config_DB as dbConf  # @UnresolvedImport   
 import SSI7X.Static.errors as errors  # @UnresolvedImport
 import SSI7X.Static.labels as labels  # @UnresolvedImport
-import SSI7X.Static.config_DB as dbConf # @UnresolvedImport   
-import SSI7X.Static.config as conf  # @UnresolvedImport 
 import SSI7X.Static.opciones_higia as optns  # @UnresolvedImport
-import time,json,jwt
 from SSI7X.ValidacionSeguridad import ValidacionSeguridad  # @UnresolvedImport
 
 '''
-Declaracion de variables globales
+    Declaracion de variables globales
+    
 '''
+
 Utils = Utils()
 validacionSeguridad = ValidacionSeguridad()
-fecha_act = time.ctime()
-C = ConnectDB()
+ld_fcha_actl = time.ctime()
+Pconnection = ConnectDB()
+
+'''
+    class Acceso
+    @author: Oscar.Daza
+    @since:02-02-2018
+    @summary: Acceso a envio de campos para crear pregunta
+    @param Form:Parametro Formulario que recibe campos validados
+'''
 
 
 class Acceso(Form):
-    cdgo = StringField(labels.lbl_cdgo,[validators.DataRequired(message=errors.ERR_NO_CDGO_PRGNTA)])
-    dscrpcn = StringField(labels.lbl_prgnta,[validators.DataRequired(message=errors.ERR_NO_PRGTA)])
+    lc_cdgo = StringField(labels.lbl_cdgo, [validators.DataRequired(message=errors.ERR_NO_CDGO_PRGNTA)])
+    lc_dscrpcn = StringField(labels.lbl_prgnta, [validators.DataRequired(message=errors.ERR_NO_PRGTA)])
+
+
+'''
+    class ActualizarAcceso
+    @author: Oscar.Daza
+    @since:02-02-2018
+    @summary: Acceso a envio de campos para actualizar pregunta
+    @param Form:Parametro Formulario que recibe campos validados para actualizacion del registro
+    @return:N/A
+
+'''
+
 
 class ActualizarAcceso(Form):
-    id_prgnta_ge = StringField(labels.lbl_nmbr_usrs,[validators.DataRequired(message=errors.ERR_NO_SN_PRMTRS)])
-    cdgo = StringField(labels.lbl_lgn,[validators.DataRequired(message=errors.ERR_NO_CDGO_PRGNTA)]) 
-    dscrpcn = StringField(labels.lbl_prgnta,[validators.DataRequired(message=errors.ERR_NO_PRGTA)])
-  
+    ln_id_prgnta_ge = StringField(labels.lbl_id_prgnta_ge, [validators.DataRequired(message=errors.ERR_NO_SN_PRMTRS)])
+    #ln_id_prgnta = StringField(labels.lbl_id_prgnta, [validators.DataRequired(message=errors.ERR_NO_SN_PRMTRS)])
+    lc_cdgo = StringField(labels.lbl_cdgo, [validators.DataRequired(message=errors.ERR_NO_CDGO_PRGNTA)]) 
+    lc_dscrpcn = StringField(labels.lbl_prgnta, [validators.DataRequired(message=errors.ERR_NO_PRGTA)])
+
+
+
+'''
+    class Preguntas
+    @author: Oscar.Daza
+    @since:02-02-2018
+    @summary:Clase que contiene metodos de funcionalidades CRUD de preguntas de seguridad
+    @param Form:Parametro Formulario que recibe recurso que provee la API
+    @return:N/A,  no aplican parametros 
+
+'''
+
     
 class Preguntas(Resource):
-   
+    '''
+        def post
+        @author: Oscar.Daza
+        @since:02-02-2018
+        @summary: metodo que retorna funcionalidad acorde a la ruta asociada
+        @param Form:
+        @return:retorna metodo de la ruta
+    '''
   
-    def post(self,**kwargs):
+    def post(self, **kwargs):
        
-        if kwargs['page'] == 'listar_preguntasg':
-            return self.ObtenerPreguntas()
-        elif kwargs['page'] == 'insertar_preguntasg':
-            return self.crearPregunta()
-        elif kwargs['page'] == 'actualizar_preguntasg':
-            return self.ActualizarPreguntas() 
-                       
-    def crearPregunta(self):
-        
-        ln_opcn_mnu = request.form["id_mnu_ge"]
-        token = request.headers['Authorization']
-        validacionSeguridad = ValidacionSeguridad()
-        val = validacionSeguridad.Principal(token,ln_opcn_mnu,optns.OPCNS_MNU['PreguntaSg'])
-        u=Acceso(request.form)
-        if not u.validate():
-            return self.Utils.nice_json({"error":u.errors},400)
-        if val:
-            DatosUsuarioToken = jwt.decode(token, conf.SS_TKN_SCRET_KEY, 'utf-8')
-            datosUsuario = validacionSeguridad.ObtenerDatosUsuario(DatosUsuarioToken['lgn'])[0]
-            arrayValues={}
-            arrayValues2={}
-            arrayValues['cdgo']=request.form['cdgo']
-            arrayValues['dscrpcn']=request.form['dscrpcn']
-            arrayValues['fcha_crcn']=str(fecha_act)
-            arrayValues['fcha_mdfccn']=str(fecha_act)
-            arrayValues['id_lgn_crcn_ge']=str(datosUsuario['id_lgn_ge'])
-            arrayValues['id_lgn_mdfccn_ge']=str(datosUsuario['id_lgn_ge'])  
-            #validacion para evitar registros duplicados 
-            Cursor = C.querySelect(dbConf.DB_SHMA +'.tbpreguntas_seguridad', 'cdgo', "cdgo='"+str(arrayValues['cdgo'])+"' or dscrpcn like '%"+str(arrayValues['dscrpcn'])+"%' ")
-            if Cursor :
-                return Utils.nice_json({"error":errors.ERR_RGSTRO_RPTDO},400)
-            id_prgnta=self.crearPregunta_seguridad(arrayValues,'tbpreguntas_seguridad')
-            arrayValues2['id_prgnta_sgrdd']=str(id_prgnta)
-            arrayValues2['id_lgn_crcn_ge']=str(datosUsuario['id_lgn_ge'])
-            arrayValues2['id_lgn_mdfccn_ge']=str(datosUsuario['id_lgn_ge'])
-            arrayValues2['fcha_crcn']=str(fecha_act)
-            arrayValues2['fcha_mdfccn']=str(fecha_act)
-            arrayValues2['id_lgn_ge']=str(datosUsuario['id_lgn_ge'])
-            self.crearPregunta_seguridad(arrayValues2,'tbpreguntas_seguridad_ge')
-            
-            return Utils.nice_json({"error":labels.SCCSS_RGSTRO_EXTSO},200)
-        return Utils.nice_json({"error":errors.ERR_NO_ATRZCN},400)       
+        if kwargs['page'] == 'listar':
+            return self.listar()
+        elif kwargs['page'] == 'crear':
+            return self.crear()
+        elif kwargs['page'] == 'actualizar':
+            return self.actualizar() 
+     
+    '''
+        def crear
+        @author: Oscar.Daza
+        @since:02-02-2018
+        @summary: metodo que permite crear preguntas de seguridad, a partir de la autorizacion que contiene el headers del token y el permiso de la accion
+        @param: 
+        @return:retorna Json del objeto creado
     
-    def ObtenerPreguntas(self): 
+    '''
+                       
+    def crear(self):
+        
         ln_opcn_mnu = request.form["id_mnu_ge"]
         token = request.headers['Authorization']
         validacionSeguridad = ValidacionSeguridad()
-        val = validacionSeguridad.Principal(token,ln_opcn_mnu,optns.OPCNS_MNU['PreguntaSg'])
-        arrayParametros={}
-        prmtrs=''
+        lb_val = validacionSeguridad.Principal(token, ln_opcn_mnu, optns.OPCNS_MNU['PreguntaSg'])
+        u = Acceso(request.form)
+        if not u.validate():
+            return self.Utils.nice_json({"error":u.errors}, 400)
+        if lb_val:
+            DatosUsuarioToken = jwt.decode(token, conf.SS_TKN_SCRET_KEY, 'utf-8')
+            lc_datosUsuario = validacionSeguridad.ObtenerDatosUsuario(DatosUsuarioToken['lgn'])[0]
+            a_prgnta = {}
+            a_prgnta_ge = {}
+            a_prgnta['cdgo'] = request.form['lc_cdgo']
+            a_prgnta['dscrpcn'] = request.form['lc_dscrpcn']
+            a_prgnta['fcha_crcn'] = str(ld_fcha_actl)
+            a_prgnta['fcha_mdfccn'] = str(ld_fcha_actl)
+            a_prgnta['id_lgn_crcn_ge'] = str(lc_datosUsuario['id_lgn_ge'])
+            a_prgnta['id_lgn_mdfccn_ge'] = str(lc_datosUsuario['id_lgn_ge'])  
+            # validacion para evitar registros duplicados 
+            Cursor = Pconnection.querySelect(dbConf.DB_SHMA + '.tbpreguntas_seguridad', 'cdgo', "cdgo='" + str(a_prgnta['cdgo']) + "' or dscrpcn like '%" + str(a_prgnta['dscrpcn']) + "%' ")
+            if Cursor :
+                return Utils.nice_json({"error":errors.ERR_RGSTRO_RPTDO}, 400)
+            ln_id_prgnta = self.crearPregunta_seguridad(a_prgnta, 'tbpreguntas_seguridad')
+            a_prgnta_ge['id_prgnta_sgrdd'] = str(ln_id_prgnta)
+            a_prgnta_ge['id_lgn_crcn_ge'] = str(lc_datosUsuario['id_lgn_ge'])
+            a_prgnta_ge['id_lgn_mdfccn_ge'] = str(lc_datosUsuario['id_lgn_ge'])
+            a_prgnta_ge['fcha_crcn'] = str(ld_fcha_actl)
+            a_prgnta_ge['fcha_mdfccn'] = str(ld_fcha_actl)
+            a_prgnta_ge['id_lgn_ge'] = str(lc_datosUsuario['id_lgn_ge'])
+            self.crearPregunta_seguridad(a_prgnta_ge, 'tbpreguntas_seguridad_ge')
+            
+            return Utils.nice_json({"success":labels.SCCSS_RGSTRO_EXTSO, }, 200)
+        return Utils.nice_json({"error":errors.ERR_NO_ATRZCN}, 400)       
+    
+    '''
+        def listar
+        @author: Oscar.Daza
+        @since:02-02-2018
+        @summary: metodo que permite listar preguntas de seguridad, a partir de la autorizacion que contiene el headers del token y el permiso de la accion
+        @param: 
+        @return:retorna objetos listados
+    
+    '''
+    
+    def listar(self): 
+        ln_opcn_mnu = request.form["id_mnu_ge"]
+        token = request.headers['Authorization']
+        validacionSeguridad = ValidacionSeguridad()
+        lb_val = validacionSeguridad.Principal(token, ln_opcn_mnu, optns.OPCNS_MNU['PreguntaSg'])
+        a_prmtrs = {}
+        lc_prmtrs = ''
         try:
-            arrayParametros['cdgo']=request.form['cdgo']
-            cdgo = arrayParametros['cdgo']
-            prmtrs += "  and a.cdgo like '%" + cdgo+"%'"
+            a_prmtrs['cdgo'] = request.form['cdgo']
+            lc_cdgo = a_prmtrs['cdgo']
+            lc_prmtrs += "  and a.cdgo like '%" + lc_cdgo + "%'"
         except:
             pass
         try: 
-            arrayParametros['dscrpcn']=request.form['dscrpcn']
-            dscrpcn=arrayParametros['dscrpcn']
-            prmtrs += "  and a.dscrpcn like '%" + dscrpcn + "%' "
+            a_prmtrs['dscrpcn'] = request.form['dscrpcn']
+            lc_dscrpcn = a_prmtrs['dscrpcn']
+            lc_prmtrs += "  and a.dscrpcn like '%" + lc_dscrpcn + "%' "
         except:
             pass
         
         try: 
-            arrayParametros['id_prgnta_ge']=request.form['id_prgnta_ge']
-            id_prgnta_ge=arrayParametros['id_prgnta_ge']
-            prmtrs += "  and b.id = '" +id_prgnta_ge+ "'"
+            
+            a_prmtrs['id_prgnta_ge'] = request.form['id_prgnta_ge']
+            ln_id_prgnta_ge = a_prmtrs['id_prgnta_ge']
+            lc_prmtrs += "  and b.id = '" + ln_id_prgnta_ge + "'"
         except:
             pass
         
-        if val:
+        if lb_val:
         
-            StrSql=" select "\
+            StrSql = " select "\
                                 " b.id,a.cdgo,a.dscrpcn "\
                                 " from "\
                                 " ssi7x.tbpreguntas_seguridad a inner join ssi7x.tbpreguntas_seguridad_ge b on "\
                                 " a.id=b.id_prgnta_sgrdd "\
                                 " where "\
                                 " b.estdo = true "\
-                                + str(prmtrs)
+                                + str(lc_prmtrs)
             print(StrSql)
-            Cursor = C.queryFree(StrSql)
+            Cursor = Pconnection.queryFree(StrSql)
             if  Cursor :    
                 data = json.loads(json.dumps(Cursor, indent=2))
-                return Utils.nice_json(data,200)
+                return Utils.nice_json(data, 200)
             else:
-                return Utils.nice_json({"error":errors.ERR_NO_RGSTRS},400)  
+                return Utils.nice_json({"error":errors.ERR_NO_RGSTRS}, 400)  
         else:
-            return Utils.nice_json({"error":errors.ERR_NO_ATRZCN},400)      
+            return Utils.nice_json({"error":errors.ERR_NO_ATRZCN}, 400)      
     
-    def ActualizarPreguntas(self):
+    '''
+        def actualizar
+        @author: Oscar.Daza
+        @since:02-02-2018
+        @summary: metodo que permite actualizar preguntas de seguridad, a partir de la autorizacion que contiene el headers del token y el permiso de la accion
+        @param: 
+        @return:retorna objetos listados
+    
+    '''
+    
+    def actualizar(self):
+        from builtins import str
+        from _ast import Str
         token = request.headers['Authorization']
         ln_opcn_mnu = request.form["id_mnu_ge"]
         validacionSeguridad = ValidacionSeguridad()
-        val = validacionSeguridad.Principal(token,ln_opcn_mnu,optns.OPCNS_MNU['PreguntaSg'])
+        lb_val = validacionSeguridad.Principal(token, ln_opcn_mnu, optns.OPCNS_MNU['PreguntaSg'])
         u = ActualizarAcceso(request.form)
         if not u.validate():
-            return Utils.nice_json({"error":u.errors},400)
-        if val :
+            return Utils.nice_json({"error":u.errors}, 400)
+        if lb_val :
             DatosUsuarioToken = jwt.decode(token, conf.SS_TKN_SCRET_KEY, 'utf-8')
-            id_lgn_ge_ssn = validacionSeguridad.ObtenerDatosUsuario(DatosUsuarioToken['lgn'])[0]
-            print (id_lgn_ge_ssn['id_lgn_ge'])
-            arrayValues={}
-            arrayValues2={}
-            #Actualizo tabla ge
-            arrayValues['id']=request.form['id_prgnta_ge']
-            arrayValues['fcha_mdfccn']=str(fecha_act)
-            arrayValues['id_lgn_mdfccn_ge']=str(id_lgn_ge_ssn['id_lgn_ge'])
-            arrayValues2['cdgo']=request.form['cdgo']
-            arrayValues2['dscrpcn']=request.form['dscrpcn']
-            arrayValues2['fcha_mdfccn']=str(fecha_act)
-            arrayValues2['id_lgn_mdfccn_ge']=str(id_lgn_ge_ssn['id_lgn_ge'])
-            #validacion para evitar registros duplicados
-            lc_tbls_query = dbConf.DB_SHMA+".tbpreguntas_seguridad_ge a INNER JOIN "+dbConf.DB_SHMA+".tbpreguntas_seguridad b on a.id_prgnta_sgrdd=b.id "
-            CursorValidar = C.querySelect(lc_tbls_query, ' b.id ', " b.cdgo = '"+str(arrayValues2['cdgo'])+"' or b.dscrpcn like '%"+str(arrayValues2['dscrpcn'])+"%'")
+            pn_id_lgn_ge_ssn = validacionSeguridad.ObtenerDatosUsuario(DatosUsuarioToken['lgn'])[0]
+            lb_estdo    = request.form["estdo"]   
+            a_prgnta_ge = {}
+            a_prgnta = {}
+            # Actualizo tabla ge
+            a_prgnta_ge['id'] = request.form['ln_id_prgnta_ge']
+            a_prgnta_ge['fcha_mdfccn'] = str(ld_fcha_actl)
+            a_prgnta_ge['id_lgn_mdfccn_ge'] = str(pn_id_lgn_ge_ssn['id_lgn_ge'])
+            a_prgnta['cdgo'] = request.form['lc_cdgo']
+            a_prgnta['dscrpcn'] = request.form['lc_dscrpcn']
+            a_prgnta['estdo']=str(lb_estdo)
+            a_prgnta['fcha_mdfccn'] = str(ld_fcha_actl)
+            a_prgnta['id_lgn_mdfccn_ge'] = str(pn_id_lgn_ge_ssn['id_lgn_ge'])
+            # validacion para evitar registros duplicados, se verifica que el codigo y la descripcion no existan en otros registros
+            lc_tbls_query = dbConf.DB_SHMA + ".tbpreguntas_seguridad_ge a INNER JOIN " + dbConf.DB_SHMA + ".tbpreguntas_seguridad b on a.id_prgnta_sgrdd=b.id "
+            CursorValidar = Pconnection.querySelect(lc_tbls_query, ' b.id ', "b.cdgo = '" + str(a_prgnta['cdgo']) + "' or b.dscrpcn like '%" + str(a_prgnta['dscrpcn']) + "%'")
             if CursorValidar:
-                return Utils.nice_json({"error":errors.ERR_RGSTRO_RPTDO},400) 
-            self.PreguntaActualizaRegistro(arrayValues,'tbpreguntas_seguridad_ge')
-            #obtengo id_prgnta a partir del id
-            Cursor = C.querySelect(dbConf.DB_SHMA +'.tbpreguntas_seguridad_ge', 'id_prgnta_sgrdd', "id="+str(request.form['id_prgnta_ge']))
+                return Utils.nice_json({"error":errors.ERR_RGSTRO_RPTDO}, 400) 
+            self.PreguntaActualizaRegistro(a_prgnta_ge, 'tbpreguntas_seguridad_ge')
+            # obtengo id_prgnta a partir del id
+            Cursor = Pconnection.querySelect(dbConf.DB_SHMA + '.tbpreguntas_seguridad_ge', 'id_prgnta_sgrdd', "id=" + str(request.form['ln_id_prgnta_ge']))
             if Cursor :
                 data = json.loads(json.dumps(Cursor[0], indent=2))
-                id_prgntasg = data['id_prgnta_sgrdd']    
-            #Actualizo tabla principal
-            arrayValues2['id']=id_prgntasg            
-            self.PreguntaActualizaRegistro(arrayValues2,'tbpreguntas_seguridad')
-            return Utils.nice_json({"error":labels.SCCSS_ACTLZCN_EXTSA},200) 
+                ln_id_prgnta = data['id_prgnta_sgrdd']    
+            # Actualizo tabla principal
+            a_prgnta['id'] = ln_id_prgnta           
+            self.PreguntaActualizaRegistro(a_prgnta, 'tbpreguntas_seguridad')
+            return Utils.nice_json({"error":labels.SCCSS_ACTLZCN_EXTSA}, 200) 
         else:
-            return Utils.nice_json({"error":errors.ERR_NO_ATRZCN},400)
+            return Utils.nice_json({"error":errors.ERR_NO_ATRZCN}, 400)
+             
+        
             
-    def crearPregunta_seguridad(self,objectValues,table_name):
-        return C.queryInsert(dbConf.DB_SHMA+"."+str(table_name), objectValues,'id')  
+    def crearPregunta_seguridad(self, objectValues, table_name):
+        return Pconnection.queryInsert(dbConf.DB_SHMA + "." + str(table_name), objectValues, 'id')  
     
-    def PreguntaActualizaRegistro(self,objectValues,table_name):
-        return C.queryUpdate(dbConf.DB_SHMA+"."+str(table_name), objectValues,'id='+str(objectValues['id']))
-    
-    
+    def PreguntaActualizaRegistro(self, objectValues, table_name):
+        return Pconnection.queryUpdate(dbConf.DB_SHMA + "." + str(table_name), objectValues, 'id=' + str(objectValues['id']))
+
